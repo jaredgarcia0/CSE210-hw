a64a6f2f66d55ec6e8b84a0a2aecc91f92746c96
= <jaredgarcia45777@gmail.com>
2025-01-17 00:47:31 -0700

    finished hw

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(0, 101);
+
+        // Console.Write("What is the magic number? ");
+        // string StringNumber = Console.ReadLine();
+        // int MagicNumber = int.Parse(StringNumber);
+
+        int guess = 0;
+
+        int numberGuess = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string StringGuess = Console.ReadLine();
+            guess = int.Parse(StringGuess);
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+                numberGuess++;
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("lower");
+                numberGuess++;
+            }
+        }
+        Console.WriteLine("You guessed it!");
+        System.Console.WriteLine($"Number of guesses were  {numberGuess} ");
     }
+
 }
